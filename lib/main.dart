386fa03b1cb53ee091a398bbd294ad9d63b3270f
TRBC <therealbitcoin.club@protonmail.com>
2019-09-07 16:01:07 +0200

    add venezuelan numbers

--- lib/main.dart
+++ lib/main.dart
@@ -52,8 +52,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  void navigate(String content) {
-    setState(() {});
+  String _homeState = "HOME";
+  void _navigate(String id) {
+    updateHomeState(id);
     Navigator.pop(context);
   }
 
@@ -115,61 +116,106 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Image.asset("images/drawer.png"),
               decoration: BoxDecoration(color: Colors.white),
             ),
-            ListTile(
-              contentPadding: EdgeInsets.all(16.0),
-              leading: Image.asset("images/venezuela.png"),
-              trailing: Icon(Icons.arrow_forward_ios),
-              title: buildDrawerItemText(context, 'Venezuela'),
-              onTap: () {
-                navigate("v");
-              },
-            ),
-            ListTile(
-              contentPadding: EdgeInsets.all(16.0),
-              leading: Image.asset("images/colombia.png"),
-              trailing: Icon(Icons.arrow_forward_ios),
-              title: buildDrawerItemText(context, 'Colombia'),
-              onTap: () {
-                navigate("c");
-              },
-            ),
-            ListTile(
-              contentPadding: EdgeInsets.all(16.0),
-              leading: Image.asset("images/usa.png"),
-              trailing: Icon(Icons.arrow_forward_ios),
-              title: buildDrawerItemText(context, 'U.S.A.'),
-              onTap: () {
-                navigate("u");
-              },
-            )
+            _buildListTileMenu(context, "venezuela", 'Venezuela', "VES"),
+            _buildListTileMenu(context, "colombia", 'Colombia', "COP"),
+            _buildListTileMenu(context, "usa", 'U.S.A.', "USD"),
           ],
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ListTile(
-              contentPadding: EdgeInsets.fromLTRB(96.0, 32.0, 32.0, 32.0),
-              leading: Image.asset("images/venezuela.png"),
-              title: buildDrawerItemText(context, 'Venezuela'),
-              onTap: () {},
-            ),
-            ListTile(
-              contentPadding: EdgeInsets.fromLTRB(96.0, 32.0, 32.0, 32.0),
-              leading: Image.asset("images/colombia.png"),
-              title: buildDrawerItemText(context, 'Colombia'),
-              onTap: () {},
-            ),
-            ListTile(
-              contentPadding: EdgeInsets.fromLTRB(96.0, 32.0, 32.0, 32.0),
-              leading: Image.asset("images/usa.png"),
-              title: buildDrawerItemText(context, 'U.S.A.'),
-              onTap: () {},
-            )
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: _buildContent(),
       ),
+    );
+  }
+
+  ListTile _buildListTileHomeContent(BuildContext context, String img,
+      String text, String id, String countryCode) {
+    return ListTile(
+      contentPadding: EdgeInsets.fromLTRB(48.0, 32.0, 48.0, 32.0),
+      leading: _drawImage(img),
+      trailing: Text(
+        countryCode,
+        style: Theme.of(context).textTheme.title,
+      ),
+      title: _buildHomeItemText(context, text),
+      onTap: () {
+        updateHomeState(id);
+      },
+    );
+  }
+
+  void updateHomeState(String id) {
+    setState(() {
+      _homeState = id;
+    });
+  }
+
+  Image _drawImage(String img) => Image.asset("images/" + img + ".png");
+
+  ListTile _buildListTileMenu(
+      BuildContext context, String img, String text, String id) {
+    return ListTile(
+      contentPadding: EdgeInsets.all(16.0),
+      leading: _drawImage(img),
+      trailing: Icon(Icons.arrow_forward_ios),
+      title: _buildDrawerItemText(context, text),
+      onTap: () {
+        _navigate(id);
+      },
+    );
+  }
+
+  List<Widget> _buildContent() {
+    List<Widget> rtv = [];
+
+    switch (_homeState) {
+      case "HOME":
+        rtv.add(Padding(padding: EdgeInsets.all(30.0)));
+        rtv.add(_buildListTileHomeContent(
+            context, "venezuela", 'Venezuela', "VES", "+58"));
+        rtv.add(_buildListTileHomeContent(
+            context, "colombia", 'Colombia', "COP", "+57"));
+        rtv.add(
+            _buildListTileHomeContent(context, "usa", 'U.S.A.', "USD", "+1"));
+        break;
+      case "VES":
+        rtv.add(_buildListTileHomeContent(
+            context, "venezuela", 'Venezuela', "", ""));
+        rtv.add(_buildListTileNumber("movilnet", "0424-291-72-09"));
+        rtv.add(_buildListTileNumber("digitel", "99-10"));
+        rtv.add(_buildListTileNumber("movistar", "34-57"));
+        break;
+      case "COP":
+        rtv.add(
+            _buildListTileHomeContent(context, "colombia", 'Colombia', "", ""));
+        break;
+      case "USD":
+        rtv.add(_buildListTileHomeContent(context, "usa", 'U.S.A.', "", ""));
+        break;
+    }
+
+    return rtv;
+  }
+
+  ListTile _buildListTileNumber(String img, String phoneNumber) {
+    return ListTile(
+      contentPadding: EdgeInsets.fromLTRB(16.0, 24.0, 16.0, 24.0),
+      leading: Image.asset(
+        "images/" + img + ".png",
+        width: 100.0,
+      ),
+      title: Text(
+        phoneNumber,
+        style: Theme.of(context).textTheme.title,
+      ),
+      trailing: Icon(
+        Icons.sms,
+        size: 36.0,
+      ),
+      onTap: () {
+        _launchURL("sms:" + phoneNumber);
+      },
     );
   }
 
@@ -181,8 +227,13 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  Text buildDrawerItemText(BuildContext context, String txt) => Text(
+  Text _buildDrawerItemText(BuildContext context, String txt) => Text(
         txt,
         style: Theme.of(context).textTheme.button,
+      );
+
+  Text _buildHomeItemText(BuildContext context, String txt) => Text(
+        txt,
+        style: Theme.of(context).textTheme.headline,
       );
 }
